bbdc97be5bd7402c30df1503209e62fbac07b576
Dan Sehl <dan.sehl@gmail.com>
2020-09-01 07:37:06 -0600

    Add validations to item model, associated test

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,9 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :unit_price, presence: true
+  validates :merchant_id, presence: true
 end
