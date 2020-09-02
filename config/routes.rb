95f63920143e7ab35e231d1cfe66e7e015660839
Dan Sehl <dan.sehl@gmail.com>
2020-09-02 15:08:55 -0600

    Add routes to find a single merchant or item

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, only: %i[show index create destroy update]
-      resources :merchants, only: %i[show index create destroy update]
 
       namespace :items do
         get '/:id/merchant', to: 'merchants#show'
+        get '/find', to: 'find#show'
       end
+      resources :items, only: %i[show index create destroy update]
 
       namespace :merchants do
         get '/:id/items', to: 'items#index'
+        get '/find', to: 'find#show'
       end
+      resources :merchants, only: %i[show index create destroy update]
     end
   end
 end
