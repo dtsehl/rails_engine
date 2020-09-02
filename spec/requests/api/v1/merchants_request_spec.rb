3bef259a17b4c7eb079d5795236750bcccd48b5b
Dan Sehl <dan.sehl@gmail.com>
2020-09-01 19:08:13 -0600

    Add relationships test for merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -99,4 +99,23 @@ RSpec.describe 'Api::V1::Merchants', type: :request do
       expect(response).to have_http_status(404)
     end
   end
+
+  describe 'Relationships' do
+    it 'can get items for a merchant' do
+      get '/api/v1/merchants/99/items'
+      json = JSON.parse(response.body, symbolize_names: true)
+      expected_ids =
+        [
+          2397, 2398, 2399, 2400, 2401, 2402, 2403, 2404, 2405, 2406,
+          2407, 2408, 2409, 2410, 2411, 2412, 2413, 2414, 2415, 2416,
+          2417, 2418, 2419, 2420, 2421, 2422, 2423, 2424, 2425, 2426,
+          2427, 2428, 2429, 2430, 2431, 2432, 2433, 2434, 2435, 2436,
+          2437, 2438
+        ]
+      item_ids = json[:data].map do |item|
+        item[:id].to_i
+      end
+      expect(item_ids.sort).to eq(expected_ids)
+    end
+  end
 end
