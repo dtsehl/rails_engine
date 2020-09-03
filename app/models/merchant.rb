0e472f79c9a4b9f72f908186971dba68d9b4cb4b
Dan Sehl <dan.sehl@gmail.com>
2020-09-03 07:57:57 -0600

    Add most revenue/items class methods to Merchant, remove searchable methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,6 @@
 class Merchant < ApplicationRecord
+  extend Searchable
+
   has_many :items
   has_many :invoices
   has_many :invoice_items, through: :items
@@ -7,35 +9,21 @@ class Merchant < ApplicationRecord
 
   validates :name, presence: true
 
-  def self.search_single_by_string(params)
-    find_by("lower(#{params.keys.first}) like ?", "%#{params.values.first.downcase}%")
+  def self.most_revenue(quantity)
+    joins(invoice_items: :transactions)
+      .select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as total_revenue')
+      .where("transactions.result='success'")
+      .group(:id)
+      .order('total_revenue desc')
+      .limit(quantity)
   end
 
-  def self.search_single_by_num(params)
-    where("#{params.keys.first} = #{params.values.first}").first
-  end
-
-  def self.search_single_by_date(params)
-    if params[:created_at].present?
-      find_by_created_at(params[:created_at])
-    else
-      find_by_updated_at(params[:updated_at])
-    end
-  end
-
-  def self.search_multiple_by_string(params)
-    where("lower(#{params.keys.first}) like ?", "%#{params.values.first.downcase}%")
-  end
-
-  def self.search_multiple_by_num(params)
-    where("#{params.keys.first} = #{params.values.first}")
-  end
-
-  def self.search_multiple_by_date(params)
-    if params[:created_at].present?
-      where(created_at: params[:created_at])
-    else
-      where(updated_at: params[:updated_at])
-    end
+  def self.most_items(quantity)
+    joins(invoice_items: :transactions)
+      .select('merchants.*, sum(invoice_items.quantity) as items_sold')
+      .where("transactions.result='success'")
+      .group(:id)
+      .order('items_sold desc')
+      .limit(quantity)
   end
 end
