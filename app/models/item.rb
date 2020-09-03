ad190a90ceb39a66072caaadf1dc0f208137ac32
Dan Sehl <dan.sehl@gmail.com>
2020-09-03 07:58:09 -0600

    Remove searchable methods in item class

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  extend Searchable
+
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
@@ -8,36 +10,4 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :unit_price, presence: true
   validates :merchant_id, presence: true
-
-  def self.search_single_by_string(params)
-    find_by("lower(#{params.keys.first}) like ?", "%#{params.values.first.downcase}%")
-  end
-
-  def self.search_single_by_num(params)
-    where("#{params.keys.first} = #{params.values.first}").first
-  end
-
-  def self.search_single_by_date(params)
-    if params[:created_at].present?
-      find_by_created_at(params[:created_at])
-    else
-      find_by_updated_at(params[:updated_at])
-    end
-  end
-
-  def self.search_multiple_by_string(params)
-    where("lower(#{params.keys.first}) like ?", "%#{params.values.first.downcase}%")
-  end
-
-  def self.search_multiple_by_num(params)
-    where("#{params.keys.first} = #{params.values.first}")
-  end
-
-  def self.search_multiple_by_date(params)
-    if params[:created_at].present?
-      where(created_at: params[:created_at])
-    else
-      where(updated_at: params[:updated_at])
-    end
-  end
 end
